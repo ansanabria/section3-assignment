fb80c3ce3316373a22c0b3f86d88647b6997c9bf
Andrés Sanabria <108693080+ansanabria@users.noreply.github.com>
2024-06-06 17:22:20 -0500

    Exercise with corrections

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const app = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
 
-  res.write("<html>");
   if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
     res.write("<h1>Hi Sam!</h1>");
     res.write(
       "<form action='/create-user' method='POST'><input type='text' name='username'><button type='submit'>Send</button></form>"
     );
+    res.write("</html>");
   } else if (url === "/users") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
     res.write(
       "<ul><li>User 1</li><li>User 2</li><li>User 3</li><li>User 4</li><li>User 5</li></ul>"
     );
+    res.write("</html>");
   } else if (url === "/create-user" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
@@ -21,10 +26,15 @@ const app = http.createServer((req, res) => {
     });
     req.on("end", () => {
       const data = Buffer.concat(body).toString();
-      console.log(data);
+      console.log(data.split("=")[1]);
     });
+    res.writeHead(302, { Location: "/" });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<p>Page not found</p>");
+    res.write("</html>");
   }
-  res.write("</html>");
   res.end();
 });
 
